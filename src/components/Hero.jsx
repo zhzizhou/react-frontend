10edd74e01333b1fc6a4e3024029c977efe38647
zhzizhou <51120720+zhzizhou@users.noreply.github.com>
2023-07-02 17:46:55 +1000

    fixed center bugging issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col'>
-            <p className='text-purple'>GROWING WITH DATA ANALYTICS</p>
-            <h1>Grow with data. </h1>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-purple font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+            <h1 className='md:text-7xl sm:text-6xl font-bold md:py-6'>Grow with data. </h1>
             <div>
                 <p>Fast, flexible financing for</p>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,19 +20,19 @@ const Navbar = () => {
             <li className='p-4'>About</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div onClick={handleNav} className='block md:hidden '>
+        <div onClick={handleNav} className='block md:hidden'>
             {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
           
 
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[40%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500': 'fixed left-[-100%]'}>
+        <div className={nav ? 'fixed left-0 top-0 w-[40%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500': 'ease-in-out duration-500 fixed left-[-100%]'}>
           <h1 className="text-3xl font-bold w-full text-purple m-3">REACT. LAB</h1>
           <ul className='p-4 uppercase '>
             <li className='p-4 border-b border-gray-600'>Home</li>
             <li className='p-4 border-b border-gray-600'>Company</li>
             <li className='p-4 border-b border-gray-600'>Resources</li>
             <li className='p-4 border-b border-gray-600'>About</li>
-            <li className='p-4 border-b border-gray-600'>Contact</li>
+            <li className='p-4'>Contact</li>
           </ul>
         </div>
     
